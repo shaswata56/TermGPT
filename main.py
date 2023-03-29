666632a91aead4ad097fb705ab8a87c110293b94
shaswata56 <shaswata56@gmail.com>
2023-03-30 00:58:00 +0600

    Terminal chatbot working as expected

---
+++ main.py
@@ -0,0 +1,26 @@
+
+import os
+import openai
+from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=Path('./personal_openai.env'))
+openai.organization = os.getenv('OPENAI_ORGANIZATION')
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+system_role = 'Act as a highly intelligent assistant.'
+prompt = 'Greetings!'
+
+while prompt != 'exit':
+    chat = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo', 
+        messages=[
+            {
+                'role': 'system', 
+                'content': system_role
+            },
+            {
+                'role': 'user', 
+                'content': prompt
+            }])
+    print('\nAI: ', chat.choices[0].message.content)
+    prompt = input('\nUser: ')
